e103c7bc865009ec5c49c021b8b9212c960f77f5
Andrew Cole <andrew.cole@illallangi.com>
2012-12-25 23:17:30 +1100

    Initial Commit

---
+++ Illallangi.CountEnum/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Illallangi Enterprises">
+// Copyright (C) 2012 Illallangi Enterprises
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CountEnum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Illallangi Enterprises")]
+[assembly: AssemblyProduct("CountEnum")]
+[assembly: AssemblyCopyright("Copyright © Illallangi Enterprises 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("361c6ef7-b4e8-4b2c-a84a-a2fe0ed62855")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
